f048664b72924dd4ba69010d5b1fe66676b1098f
James Parrott <japarrott@shaw.ca>
2024-11-19 20:23:37 -0700

    build out the hangman function, grab a valid word and determine length, get input from player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+import string
 
 def get_valid_word(words):
     word = random.choice(words)
@@ -7,4 +8,14 @@ def get_valid_word(words):
         word = random.choice(words)
 
     return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+    user_letter = input("Guess a letter: ").upper()
+
+user_input = input("Type something:")
+print(user_input)
 
