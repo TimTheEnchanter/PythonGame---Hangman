4e7d9a979a0dc45ee98326239881a9f7b647c1a8
James Parrott <japarrott@shaw.ca>
2024-11-19 21:49:59 -0700

    Track letters that were used and part of the word, add responses for incorrect input, display word and word usage

--- main.py
+++ main.py
@@ -16,6 +16,22 @@ def hangman():
     used_letters = set()
     user_letter = input("Guess a letter: ").upper()
 
+    while len(word_letters) > 0:
+        print("You have guessed the letters: ", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print("Current Word: ", " ".join(word_list))
+
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in user_letter:
+            print("You have already guessed that letter.")
+        else:
+            print("Invalid character")
+
 user_input = input("Type something:")
 print(user_input)
 
