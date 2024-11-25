83dd1cf1748548492367367fa1d81b4da9ee2b2d
James Parrott <japarrott@shaw.ca>
2024-11-25 14:40:00 -0700

    add a limited amount of guesses to the game

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
     user_letter = input("Guess a letter: ").upper()
+    lives = 6
 
-    while len(word_letters) > 0:
-        print("You have guessed the letters: ", " ".join(used_letters))
+    while len(word_letters) > 0 and lives > 0:
+        print("You have", lives," left. You have guessed the letters: ", " ".join(used_letters))
 
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print("Current Word: ", " ".join(word_list))
@@ -26,6 +27,9 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print("Letter is not in word.")
 
         elif user_letter in user_letter:
             print("You have already guessed that letter.")
